885c5555acc3af438222fdfc5edd9e2a09f0fcf2
vireax <kim@ximplex.net>
2014-01-26 10:09:19 +0700

    Create 2013-07-04-timeframe.d

---
+++ _posts/2013-07-04-timeframe.d
@@ -0,0 +1,35 @@
+---
+layout: post
+title: Timeframe
+date: 2013-07-04
+---
+
+10:00 - 10:40
+
+The scholarship will end in May 2014. 10 months left.
+>> 5 for the research, 5 for writing and administrative tasks
+This project uses CCGA to solve damage detection in truss structures
+Truss can be 2D or 3D >> Search who are working with 3D trusses
+Damage factor were assigned numerically, simulated, and compared to the solution by CCGA
+* No noise: ( as submitted to iCOME 2013)
+* With noise: (2-3%) > exist in real measurement from instrumentation or environment; to be added randomly in simulation >> Search how to include noise
+
+Some cases should be tested with standard GA
+* simulation for performance comparison needs 30+ run for statistical validity/repeatablity, although some papers did not mention this practice.
+
+Proposal
+* Committee: Dr. Kittipong, Dr. Putha, & Dr. Jitti
+* Defense around 3rd week of September (16th-18th)
+* [30 SEP - 11 OCT] will be final exam session, the committee might not be available
+
+Writing
+* Automatic indexing: table and figure caption feature in MS Word > OK, but need verification with university's formatting guideline
+* Citation: manual(direct but need to sort in excel first) or automated tool (Endnote, Zotero, or Reference Manager in Word) >> test zotero integration with Word
+* Organization may not not need to follow generic chapter title >> Check with faculty librarians
+* Word limit: not necessary, keep to the main point, no descriptive report needed
+
+Programming
+* Not necessary to work with C, yet it might be defficult
+* Find how to solve problem by including noise into the problem
+* Based on Matlab, run existing code for for beam, code truss structure, include the noise, validate with multiple run >> Be clear first with CCGA
+* ...
